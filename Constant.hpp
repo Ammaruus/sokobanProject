f5d81537e05f7a52e2e90ca5c12697209e18b1dc
Ammaruus <99613167+Ammaruus@users.noreply.github.com>
2023-08-11 23:49:02 +0200

    Delete Constant.hpp

--- Constant.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef Constant_hpp
-#define Constant_hpp
-
-const int windowWidth = 500;
-const int windowHeight = 500;
-const double refreshPerSecond = 60;
-//Number of walls for the map generation
-
-#endif
